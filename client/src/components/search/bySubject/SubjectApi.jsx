ac1368969356194c3d7e8d2dac22e8763d320935
AltaraceO <orialtarace@gmail.com>
2022-02-03 20:41:21 +0200

    create a book object through different api calls to openLibreary

--- client/src/components/search/bySubject/SubjectApi.jsx
+++ client/src/components/search/bySubject/SubjectApi.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 // import axios from "axios";
 // const url = "https://openlibrary.org";
 import url from "../../../api/openLibApi";
 
 export const SubjectApi = () => {
   const [cat, setCat] = useState("");
+  const [bookInfo, setBookInfo] = useState([]);
 
   const onHandleChange = (e) => {
     setCat(e.target.value);
@@ -13,8 +14,32 @@ export const SubjectApi = () => {
   const getOpenLib = async (e) => {
     e.preventDefault();
     const { data } = await url.get(`/subjects/${cat}.json?limit=50`);
-    console.log(data);
+    console.log(data.works);
+    setBookInfo(data.works);
   };
+
+  useEffect(() => {
+    const getMoreBookInfo = async () => {
+      bookInfo.map(async (book) => {
+        const bookObj = {
+          author: book.authors.map((auth) => auth.name),
+          title: book.title,
+          categories: book.subject,
+        };
+        const bookIsbn = await url.get(`/books/${book.cover_edition_key}.json`);
+        console.log(bookIsbn.data);
+        bookObj.pages = bookIsbn.data.number_of_pages;
+        bookObj.isbn_13 = bookIsbn.data.isbn_13;
+        bookObj.isbn_10 = bookIsbn.data.isbn_10;
+        console.log(bookObj);
+        return bookObj;
+      });
+    };
+    if (bookInfo.length) {
+      getMoreBookInfo();
+    }
+  }, [bookInfo]);
+
   return (
     <div>
       <form>
