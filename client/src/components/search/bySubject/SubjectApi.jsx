4872e667f7e0a488d615ffe20f6979a76444a0fe
AltaraceO <orialtarace@gmail.com>
2022-02-04 14:51:22 +0200

    make sure that objects information is collected in PromiseAll and displayed on the screen

--- client/src/components/search/bySubject/SubjectApi.jsx
+++ client/src/components/search/bySubject/SubjectApi.jsx
@@ -1,71 +1,84 @@
 import React, { useEffect, useState } from "react";
 import url from "../../../api/openLibApi";
-import gooUrl from "../../../api/googleApi";
+import googleUrl from "../../../api/googleApi";
 
 export const SubjectApi = () => {
   const [cat, setCat] = useState("");
-  const [bookInfo, setBookInfo] = useState([]);
-  const numberOfResults = 7;
+  const [bookRawInfo, setBookRawInfo] = useState([]);
+  const [bookResults, setBookResults] = useState("");
+  const numberOfResults = 4;
 
   const onHandleChange = (e) => {
     setCat(e.target.value);
   };
 
-  const getOpenLib = async (e) => {
+  //get books by category
+  const onHandleClick = async (e) => {
     e.preventDefault();
     const { data } = await url.get(
       `/subjects/${cat}.json?limit=${numberOfResults}`
     );
     console.log("where is this?", data.works);
-    setBookInfo(data.works);
+    setBookRawInfo(data.works);
   };
 
-  useEffect(() => {
-    const getMoreBookInfo = async () => {
-      bookInfo.map(async (book) => {
+  const makeBookObjects = async () => {
+    const allBooks = await Promise.all(
+      bookRawInfo.map(async (book) => {
         const bookObj = {
           author: book.authors.map((auth) => auth.name),
           title: book.title,
           categories: book.subject,
         };
+        //use Open Library's book key to get ISBN numbers, pages and publishers
         const bookIsbn = await url.get(`/books/${book.cover_edition_key}.json`);
         if (!bookIsbn.data.isbn_13) {
           return;
         }
-        // console.log("lib", bookIsbn.data);
-
         bookObj.pages = bookIsbn.data.number_of_pages;
         bookObj.isbn_13 = bookIsbn.data.isbn_13;
         bookObj.isbn_10 = bookIsbn.data.isbn_10;
         bookObj.publishers = bookIsbn.data.publishers.map((pub) => pub);
 
-        const bookImg = await gooUrl.get(`isbn:${bookIsbn.data.isbn_13}`);
+        //get book thumbnail and subtitle from Google by ISBN number
+        const bookImg = await googleUrl.get(`isbn:${bookIsbn.data.isbn_13}`);
         //conditional chaining when looking through objects
         if (!bookImg.data.items?.[0].volumeInfo.imageLinks) {
           return;
         }
-        // console.log("google", bookImg.data.items[0].volumeInfo);
         bookObj.thumb = bookImg.data.items[0].volumeInfo.imageLinks.thumbnail;
-
-        // console.log(bookImg.data.items[0].volumeInfo.subtitle);
         bookObj.subtitle = bookImg.data.items[0].volumeInfo.subtitle || "";
         console.log("obj", bookObj);
         return bookObj;
-      });
-    };
-    if (bookInfo.length) {
-      getMoreBookInfo();
-    }
-  }, [bookInfo]);
+      })
+    );
+    return allBooks;
+  };
 
+  useEffect(() => {
+    const getMoreBookRawInfo = async () => {
+      const books = await makeBookObjects();
+      setBookResults(books.filter((b) => b !== undefined));
+    };
+
+    if (bookRawInfo.length) {
+      getMoreBookRawInfo();
+    }
+  }, [bookRawInfo]);
+
+  console.log(bookResults);
   return (
     <div>
       <form>
         <label htmlFor="book"></label>
         <input onChange={onHandleChange} type="text" name="book" value={cat} />
         <br />
-        <button onClick={getOpenLib}>Submit</button>
+        <button onClick={onHandleClick}>Submit</button>
       </form>
+      {bookResults &&
+        bookResults.map((book, idx) => {
+          return <div key={idx}> {book.title}</div>;
+        })}
     </div>
   );
 };
