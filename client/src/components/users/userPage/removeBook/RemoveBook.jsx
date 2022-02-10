d45ba61e56f000816d886c8d269e3d5fe2ff9e29
AltaraceO <orialtarace@gmail.com>
2022-02-11 01:33:38 +0200

    heroku bookDetails tyring removeBook no api

--- client/src/components/users/userPage/removeBook/RemoveBook.jsx
+++ client/src/components/users/userPage/removeBook/RemoveBook.jsx
@@ -8,14 +8,17 @@ export const RemoveBook = ({ book, func }) => {
       Authorization: `Bearer ${localStorage.getItem("authToken")}`,
     },
   };
-  const onHandleRemove = async () => {
-    await url.post("/users/removeBook", book, config);
-    const books = await url.get("/books/getBooks", config);
-    // func(books.data);
-  };
+  // const onHandleRemove = async () => {
+  //   await url.post("/users/removeBook", book, config);
+  //   const books = await url.get("/books/getBooks", config);
+  //   // func(books.data);
+  // };
   return (
     <div>
-      <button className="remove-button" onClick={onHandleRemove}>
+      <button
+        className="remove-button"
+        //  onClick={onHandleRemove}
+      >
         Remove Book
       </button>
     </div>
