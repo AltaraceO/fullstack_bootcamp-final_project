ecb621d0ecfadd576c47990020a0c8fac58fa83b
AltaraceO <orialtarace@gmail.com>
2022-02-12 23:23:28 +0200

    homepage layout

--- client/src/components/homePage/Home.jsx
+++ client/src/components/homePage/Home.jsx
@@ -1,5 +1,89 @@
 import React from "react";
+import "./home.css";
+import { Link } from "react-router-dom";
 
 export const Home = () => {
-  return <>bye</>;
+  return (
+    <>
+      <div className="intro">
+        <div className="about">
+          This application allows a user to search for books by genres or
+          categories. When a search is implemented a list of books is fetched
+          from the Open Library book API. By using the book's ISBN codes a
+          search is immediately conducted on the Google books API in order to
+          fetch the information missing in the first call, such as, images a
+          short extract and links
+        </div>
+        <div className="details">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, enim
+          dolorum. Nostrum sint impedit deleniti eos esse consectetur suscipit
+          neque laboriosam nesciunt nihil facere ullam velit non maiores,
+          laudantium reprehenderit harum temporibus sit, consequuntur aut nam
+          sed! Quas blanditiis cupiditate corrupti harum, cumque molestiae
+          aspernatur inventore incidunt aliquam ipsam aperiam fuga, nemo porro
+          in quam beatae. Sit sapiente temporibus ab, expedita dolorum quia aut
+          exercitationem molestiae laborum laudantium. Perferendis, modi!
+        </div>
+      </div>
+      <div className="links">
+        <Link className="home-button" to="/registration">
+          Login
+        </Link>
+        <a
+          className="home-button"
+          href="https://openlibrary.org/"
+          target="_blank"
+        >
+          Open Library
+        </a>
+        <a
+          className="home-button"
+          href="https://books.google.com/"
+          target="_blank"
+        >
+          Google Books
+        </a>
+      </div>
+      <div className="about">
+        <p>
+          Genre is any form or type of communication in any mode (written,
+          spoken, digital, artistic, etc.) with socially-agreed-upon conventions
+          developed over time. In popular usage, it normally describes a
+          category of literature, music, or other forms of art or entertainment,
+          whether written or spoken, audio or visual, based on some set of
+          stylistic criteria, yet genres can be aesthetic, rhetorical,
+          communicative, or functional. Genres form by conventions that change
+          over time as cultures invent new genres and discontinue the use of old
+          ones. Often, works fit into multiple genres by way of borrowing and
+          recombining these conventions. Stand-alone texts, works, or pieces of
+          communication may have individual styles, but genres are amalgams of
+          these texts based on agreed-upon or socially inferred conventions.
+          Some genres may have rigid, strictly adhered-to guidelines, while
+          others may show great flexibility. Genre began as an absolute
+          classification system for ancient Greek literature, as set out in
+          Aristotle's Poetics. For Aristotle, poetry (odes, epics, etc.), prose,
+          and performance each had specific design features that supported
+          appropriate content of each genre. Speech patterns for comedy would
+          not be appropriate for tragedy, for example, and even actors were
+          restricted to their genre under the assumption that a type of person
+          could tell one type of story best.
+        </p>
+
+        <p>
+          Genres proliferate and develop beyond Aristotle’s classifications in
+          response to changes in audiences and creators. Genre has become a
+          dynamic tool to help the public make sense out of unpredictability
+          through artistic expression. Given that art is often a response to a
+          social state, in that people write, paint, sing, dance, and otherwise
+          produce art about what they know about, the use of genre as a tool
+          must be able to adapt to changing meanings. Genre suffers from the
+          ills of any classification system. Musician Ezra LaFleur argues that
+          discussion of genre should draw from Ludwig Wittgenstein's idea of
+          family resemblance. Genres are helpful labels for communicating but do
+          not necessarily have a single attribute that is the essence of the
+          genre.
+        </p>
+      </div>
+    </>
+  );
 };
--- client/src/components/users/userPage/UserPage.jsx
+++ client/src/components/users/userPage/UserPage.jsx
@@ -68,7 +68,7 @@ export const UserPage = () => {
 
   const renderIndividual = () => {
     return (
-      <div>
+      <>
         {sendIndivBook && (
           <BookDetails
             likeFunc={bookLike}
@@ -76,7 +76,7 @@ export const UserPage = () => {
             details={sendIndivBook}
           />
         )}
-      </div>
+      </>
     );
   };
 
